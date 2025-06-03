89feaa8c9d6e333c2354e6772f854071456b7a62
Marlon Werth <marlon.werth@dfki.de>
2025-06-03 13:28:15 +0200

    feat(navbar) add mobile settings and contact link

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from 'react';
 import Link from 'next/link';
-import { FaHome, FaBriefcase, FaFileAlt, FaCog } from 'react-icons/fa';
+import { FaHome, FaBriefcase, FaFileAlt, FaEnvelope } from 'react-icons/fa';
 
 // Localization
 import { useTranslations } from 'next-intl';
@@ -10,6 +10,7 @@ import { useTranslations } from 'next-intl';
 // Components
 import ThemeToggle from './ThemeToggle';
 import LanguageSwitcher from './LanguageSwitcher';
+import MobileSettings from './MobileSettings';
 
 interface LanguageSwitcherProps {
     initialLocale?: string;
@@ -17,77 +18,76 @@ interface LanguageSwitcherProps {
 
 const Navbar: React.FC<LanguageSwitcherProps> = ({ initialLocale }) => {
     const t = useTranslations('Navbar');
-    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-
-    const toggleDropdown = () => {
-        setIsDropdownOpen(!isDropdownOpen);
-    };
 
     return (
-        <nav className="bg-navbar p-3 pl-12 pr-12 lg:pl-16 lg:pr-16 backdrop-filter backdrop-blur-lg backdrop-opacity-100 rounded-tl-full rounded-tr-full lg:rounded-tl-none lg:rounded-tr-none z-10 shadow fixed bottom-0 inset-x-0 lg:top-0 lg:bottom-auto">
-            <div className="max-w-screen-xl mx-auto flex justify-between items-center">
-                <ul className="flex justify-between w-full md:w-auto md:justify-start space-x-4 sm:space-x-8">
-                    <li>
-                        <Link
-                            href="/"
-                            className="hover:text-gray-400 flex flex-col md:flex-row items-center"
-                        >
-                            <FaHome
-                                className="md:mr-2 text-foreground"
-                                size={25}
-                            />
-                            <span className="md:inline">{t('home')}</span>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            href="/portfolio"
-                            className="hover:text-gray-400 flex flex-col md:flex-row items-center"
-                        >
-                            <FaBriefcase
-                                className="md:mr-2 text-foreground"
-                                size={25}
-                            />
-                            <span className="md:inline">{t('portfolio')}</span>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            href="/cv"
-                            className="hover:text-gray-400 flex flex-col md:flex-row items-center"
-                        >
-                            <FaFileAlt
-                                className="md:mr-2 text-foreground"
-                                size={25}
-                            />
-                            <span className="hidden md:inline">{t('cv')}</span>
-                            <span className="block md:hidden">CV</span>
-                        </Link>
-                    </li>
-                    <li className="md:hidden">
-                        <button
-                            onClick={toggleDropdown}
-                            className="flex flex-col md:flex-row items-center"
-                        >
-                            <FaCog className="text-foreground" size={25} />
-                            <span>{t('settings')}</span>
-                        </button>
-                        {isDropdownOpen && (
-                            <div className="absolute bottom-full mb-2 right-4 bg-background p-2 rounded shadow-lg z-50">
-                                <LanguageSwitcher
-                                    initialLocale={initialLocale}
+        <>
+            <nav className="bg-navbar p-3 pl-12 pr-12 lg:pl-16 lg:pr-16 backdrop-filter backdrop-blur-lg backdrop-opacity-100 rounded-tl-full rounded-tr-full lg:rounded-tl-none lg:rounded-tr-none z-10 shadow fixed bottom-0 inset-x-0 lg:top-0 lg:bottom-auto">
+                <div className="max-w-screen-xl mx-auto flex justify-between items-center">
+                    <ul className="flex justify-between w-full md:w-auto md:justify-start space-x-4 sm:space-x-8">
+                        <li>
+                            <Link
+                                href="/"
+                                className="hover:text-gray-400 flex flex-col lg:flex-row items-center"
+                            >
+                                <FaHome
+                                    className="lg:mr-2 text-foreground"
+                                    size={25}
                                 />
-                                <ThemeToggle />
-                            </div>
-                        )}
-                    </li>
-                </ul>
-                <div className="hidden md:flex items-center space-x-4">
-                    <LanguageSwitcher initialLocale={initialLocale} />
-                    <ThemeToggle />
+                                <span className="lg:inline">{t('home')}</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href="/portfolio"
+                                className="hover:text-gray-400 flex flex-col lg:flex-row items-center"
+                            >
+                                <FaBriefcase
+                                    className="lg:mr-2 text-foreground"
+                                    size={25}
+                                />
+                                <span className="lg:inline">
+                                    {t('portfolio')}
+                                </span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href="/cv"
+                                className="hover:text-gray-400 flex flex-col lg:flex-row items-center"
+                            >
+                                <FaFileAlt
+                                    className="lg:mr-2 text-foreground"
+                                    size={25}
+                                />
+                                <span className="hidden lg:inline">
+                                    {t('cv')}
+                                </span>
+                                <span className="block lg:hidden">CV</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href="/contact"
+                                className="hover:text-gray-400 flex flex-col lg:flex-row items-center"
+                            >
+                                <FaEnvelope
+                                    className="lg:mr-2 text-foreground"
+                                    size={25}
+                                />
+                                <span className="lg:inline">
+                                    {t('contact')}
+                                </span>
+                            </Link>
+                        </li>
+                    </ul>
+                    <div className="hidden md:flex items-center space-x-4">
+                        <LanguageSwitcher initialLocale={initialLocale} />
+                        <ThemeToggle />
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+            <MobileSettings initialLocale={initialLocale} />
+        </>
     );
 };
 
